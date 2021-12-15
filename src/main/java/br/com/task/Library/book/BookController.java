1444de83b242d0ab3efbca8705cec8d7c5f8277d
Beatriz Maryah <beatrizmaryah16@gmail.com>
2021-12-15 17:31:38 -0300

    Trazendo a lógica para o service

--- src/main/java/br/com/task/Library/book/BookController.java
+++ src/main/java/br/com/task/Library/book/BookController.java
@@ -14,6 +14,9 @@ import java.util.List;
 public class BookController {
 
     @Autowired
+    BookService bookService;
+
+    @Autowired
     private BookRepository bookRepository;
 
     @Autowired
@@ -21,65 +24,17 @@ public class BookController {
 
     @PostMapping
     public ResponseEntity<BookDto> addBook(@RequestBody BookDto bookDto) {
-
-        if(!bookDto.name.isEmpty() && !bookDto.author.isEmpty()){
-            final Book bookEntity = bookMapper.toEntity(bookDto);
-            final Book bookSaved = bookRepository.save(bookEntity);
-            BookDto dto = bookMapper.toDto(bookSaved);
-
-            return new ResponseEntity<>(dto, HttpStatus.CREATED);
-        } else {
-            throw new BookNotFoundException();
-        }
+        return bookService.insertBook(bookDto);
     }
 
     @GetMapping
     public ResponseEntity<List<BookDto>> all(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "author", required = false) String author) {
-
-        List<Book> books = bookRepository.findAll();
-
-        if(name == null && author == null){
-            return ResponseEntity.ok().body(bookMapper.toDto(books));
-        } else if (name != null && author != null) {
-            List<Book> booksSameAuthorAndName = new ArrayList<>();
-            for (Book book : books) {
-
-                if (book.getName().toLowerCase().contains(name.toLowerCase()) && book.getAuthor().toLowerCase().equals(author.toLowerCase())) {
-                    booksSameAuthorAndName.add(book);
-                }
-            }
-            return new ResponseEntity<>(bookMapper.toDto(booksSameAuthorAndName), HttpStatus.OK);
-        } else if (name != null && author == null) {
-            List<Book> booksSameName = new ArrayList<>();
-
-            for (Book book : books) {
-                if (book.getName().toLowerCase().contains(name.toLowerCase())) {
-                    booksSameName.add(book);
-                }
-            }
-
-            return ResponseEntity.ok().body(bookMapper.toDto(booksSameName));
-        } else if (author != null && name == null) {
-            List<Book> booksSameAuthor = new ArrayList<>();
-
-            for(Book book : books){
-                if (book.getAuthor().toLowerCase().contains(author.toLowerCase())) {
-                    booksSameAuthor.add(book);
-                }
-            }
-            return new ResponseEntity<>(bookMapper.toDto(booksSameAuthor), HttpStatus.OK);
-        }
-        return new ResponseEntity<>(bookMapper.toDto(books), HttpStatus.OK);
+        return bookService.getBooks(name, author);
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<BookDto> getById(@PathVariable(value = "id") Long id) {
-        Book book = bookRepository.getById(id);
-        if (book != null) {
-            BookDto dto = bookMapper.toDto(book);
-            return new ResponseEntity<>(dto, HttpStatus.OK);
-        }
-        throw new BookNotFoundException();
+        return bookService.getBookById(id);
     }
 
     @PutMapping("/{id}")
---
+++ src/main/java/br/com/task/Library/book/BookService.java
@@ -0,0 +1,89 @@
+package br.com.task.Library.book;
+
+import br.com.task.Library.exception.BookNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private BookMapper bookMapper;
+
+    public ResponseEntity<BookDto> insertBook(BookDto bookDto){
+        if(!bookDto.name.isEmpty() && !bookDto.author.isEmpty()){
+            final Book bookEntity = bookMapper.toEntity(bookDto);
+            final Book bookSaved = bookRepository.save(bookEntity);
+            BookDto dto = bookMapper.toDto(bookSaved);
+
+            return new ResponseEntity<>(dto, HttpStatus.CREATED);
+        } else {
+            throw new BookNotFoundException();
+        }
+    }
+
+    public ResponseEntity<List<BookDto>> getBooks (String name, String author){
+        List<Book> books = bookRepository.findAll();
+
+        if(name == null && author == null){
+            return ResponseEntity.ok().body(bookMapper.toDto(books));
+        } else if (name != null && author != null) {
+            return getBooksSameAuthorAndName(books, name, author);
+        } else if (name != null && author == null) {
+            return getBooksSameName(books, name);
+        } else if (author != null && name == null) {
+            return getBooksSameAuthor(books, author);
+        }
+        return new ResponseEntity<>(bookMapper.toDto(books), HttpStatus.OK);
+    }
+
+    public ResponseEntity<List<BookDto>> getBooksSameAuthorAndName(List<Book> books, String name, String author){
+        List<Book> booksSameAuthorAndName = new ArrayList<>();
+
+        for (Book book : books) {
+
+            if (book.getName().toLowerCase().contains(name.toLowerCase()) && book.getAuthor().toLowerCase().equals(author.toLowerCase())) {
+                booksSameAuthorAndName.add(book);
+            }
+        }
+        return new ResponseEntity<>(bookMapper.toDto(booksSameAuthorAndName), HttpStatus.OK);
+    }
+
+    public ResponseEntity<List<BookDto>> getBooksSameName(List<Book> books, String name){
+        List<Book> booksSameName = new ArrayList<>();
+
+        for (Book book : books) {
+            if (book.getName().toLowerCase().contains(name.toLowerCase())) {
+                booksSameName.add(book);
+            }
+        }
+        return ResponseEntity.ok().body(bookMapper.toDto(booksSameName));
+    }
+    public ResponseEntity<List<BookDto>> getBooksSameAuthor(List<Book> books, String author){
+        List<Book> booksSameAuthor = new ArrayList<>();
+
+        for(Book book : books){
+            if (book.getAuthor().toLowerCase().contains(author.toLowerCase())) {
+                booksSameAuthor.add(book);
+            }
+        }
+        return new ResponseEntity<>(bookMapper.toDto(booksSameAuthor), HttpStatus.OK);
+    }
+
+    public ResponseEntity<BookDto> getBookById(Long id){
+        Book book = bookRepository.getById(id);
+        if (book != null) {
+            BookDto dto = bookMapper.toDto(book);
+            return new ResponseEntity<>(dto, HttpStatus.OK);
+        }
+        throw new BookNotFoundException();
+    }
+}
