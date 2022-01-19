0a9a4fd4016d2e76eb30080c86c3007479d03c12
Beatriz Maryah <beatrizmaryah16@gmail.com>
2022-01-19 16:22:52 -0300

    Verificando o erro

--- src/main/java/br/com/task/Library/GlobalExceptionHandler.java
+++ src/main/java/br/com/task/Library/GlobalExceptionHandler.java
@@ -12,23 +12,23 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({NotFoundException.class, LoginNotFoundException.class, WrongLoginException.class, IllegalArgumentException.class})
-    public ResponseEntity<String> handleException(Exception ex, WebRequest request){
-
-//        if(ex instanceof NullPointerException){
+//    @ExceptionHandler({NotFoundException.class, LoginNotFoundException.class, WrongLoginException.class, IllegalArgumentException.class})
+//    public ResponseEntity<String> handleException(Exception ex, WebRequest request){
+//
+////        if(ex instanceof NullPointerException){
+////            return new ResponseEntity<>("Internal system failure: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+////        } 
+//        	
+//        if(ex instanceof NotFoundException){
+//            return new ResponseEntity<>("The book was not informed: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+//        } else if(ex instanceof LoginNotFoundException){
+//            return new ResponseEntity<>("The login was not informed: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+//        } else if(ex instanceof WrongLoginException){
+//            return new ResponseEntity<>("Wrong Login: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+//        } else if(ex instanceof IllegalArgumentException){
+//            return new ResponseEntity<>("Parameters are not right: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+//        } else{
 //            return new ResponseEntity<>("Internal system failure: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
-//        } 
-        	
-        if(ex instanceof NotFoundException){
-            return new ResponseEntity<>("The book was not informed: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
-        } else if(ex instanceof LoginNotFoundException){
-            return new ResponseEntity<>("The login was not informed: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
-        } else if(ex instanceof WrongLoginException){
-            return new ResponseEntity<>("Wrong Login: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
-        } else if(ex instanceof IllegalArgumentException){
-            return new ResponseEntity<>("Parameters are not right: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
-        } else{
-            return new ResponseEntity<>("Internal system failure: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
+//        }
+//    }
 }
